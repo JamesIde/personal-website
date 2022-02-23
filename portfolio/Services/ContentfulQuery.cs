6a67c656275133be0103b0675e5dd91366a02e98
James <james.ide775@gmail.com>
2022-02-23 13:27:15 +1030

    updated contentfulQuery service to consume api

--- portfolio/Services/ContentfulQuery.cs
+++ portfolio/Services/ContentfulQuery.cs
@@ -14,20 +14,14 @@ namespace portfolio.Services
             _httpClient = httpClient;
 
         }
-
-        public Task<IEnumerable<Asset>> GetAssetByTitle(string title)
+        //Retrieve posts
+        public async Task<IEnumerable<BlogPost>> GetPostBySlug(string slug)
         {
-            throw new NotImplementedException();
-        }
+            var response = await _httpClient.GetAsync($"api/ContentfulService/GetPostBySlug?slug={slug}");
+            var content = await response.Content.ReadAsStringAsync();
+            var blogpost = JsonConvert.DeserializeObject<IEnumerable<BlogPost>>(content);
+            return blogpost;
 
-        public Task<IEnumerable<Records>> GetEntries()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<BlogPost>> GetPostBySlug(string slug)
-        {
-            throw new NotImplementedException();
         }
 
         public async Task<IEnumerable<BlogPost>> GetPosts()
@@ -38,44 +32,35 @@ namespace portfolio.Services
             return blogposts;
         }
 
-        public Task<IEnumerable<Records>> GetRecordBySlug(string slug)
+        
+
+        public async Task<IEnumerable<Records>> GetEntries()
         {
-            throw new NotImplementedException();
+            var response = await _httpClient.GetAsync($"api/ContentfulService/GetEntries");
+            var content = await response.Content.ReadAsStringAsync();
+            var records = JsonConvert.DeserializeObject<IEnumerable<Records>>(content);
+            return records;
+        }
+
+       
+
+        public async Task<IEnumerable<Records>> GetEntryBySlug(string slug)
+        {
+            var response = await _httpClient.GetAsync($"api/ContentfulService/GetEntryBySlug?slug={slug}");
+            var content = await response.Content.ReadAsStringAsync();
+            var records = JsonConvert.DeserializeObject<IEnumerable<Records>>(content);
+            return records;
+        }
+        //Retrieve assets
+        public async Task<IEnumerable<Asset>> GetAssetByTitle(string title)
+        {
+            var response = await _httpClient.GetAsync($"api/ContentfulService/GetAssetByTitle?title={title}");
+            var content = await response.Content.ReadAsStringAsync();
+            var records = JsonConvert.DeserializeObject<IEnumerable<Asset>>(content);
+            return records;
         }
 
 
 
-        //public async Task<IEnumerable<Records>> GetEntries()
-        //{
-        //    var entryString = QueryBuilder<Records>.New.ContentTypeIs("thumbnail");
-        //    var records = await _contentfulClient.GetEntries<Records>(entryString);
-        //    return records;
-        //}
-        //public async Task<IEnumerable<Records>> GetRecordBySlug(string slug)
-        //{
-        //    var entryString = QueryBuilder<Records>.New.ContentTypeIs("thumbnail").FieldEquals("fields.slug", slug);
-        //    var record = await _contentfulClient.GetEntries<Records>(entryString);
-        //    return record;
-        //}
-
-        //public async Task<IEnumerable<Asset>> GetAssetByTitle(string title)
-        //{
-        //    var titleFilter = QueryBuilder<Asset>.New.FieldEquals("fields.title", title);
-        //    var assets = await _contentfulClient.GetAssets(titleFilter);
-        //    return assets;
-        //}
-
-        //public async Task<IEnumerable<BlogPost>> GetPosts()
-        //{
-        //    var entryString = QueryBuilder<BlogPost>.New.ContentTypeIs("blogPost");
-        //    var posts = await _contentfulClient.GetEntries<BlogPost>(entryString);
-        //    return posts;
-        //}
-        //public async Task<List<BlogPost>> GetPostBySlug(string slug)
-        //{
-        //    var entryString = QueryBuilder<BlogPost>.New.ContentTypeIs("blogPost").FullTextSearch(slug);
-        //    var post = await _contentfulClient.GetEntries<BlogPost>(entryString);
-        //    return post.ToList();
-        //}
     }
 }
--- portfolio/Services/IContentfulQuery.cs
+++ portfolio/Services/IContentfulQuery.cs
@@ -7,12 +7,12 @@ namespace portfolio.Services
     {
 
         public Task<IEnumerable<Records>> GetEntries();
-        public Task<IEnumerable<Records>> GetRecordBySlug(string slug);
+        public Task<IEnumerable<Records>> GetEntryBySlug(string slug);
 
         public Task<IEnumerable<Asset>> GetAssetByTitle(string title);
 
         public Task<IEnumerable<BlogPost>> GetPosts();
-        public Task<List<BlogPost>> GetPostBySlug(string slug);
+        public Task<IEnumerable<BlogPost>> GetPostBySlug(string slug);
 
     }
 }
