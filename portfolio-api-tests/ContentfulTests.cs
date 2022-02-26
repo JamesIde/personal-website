f48b9be4d9097558078008192793d765371c0103
James <james.ide775@gmail.com>
2022-02-27 09:01:48 +1030

    Implemented httpClient getFromJsonAsync in ContentfulQuery.cs

---
+++ portfolio-api-tests/ContentfulTests.cs
@@ -0,0 +1,14 @@
+using Contentful.Core;
+using Microsoft.AspNetCore.Mvc;
+using portfolio_api.Controllers;
+using portfolio_api.Models;
+using System.Net.Http;
+using Xunit;
+
+namespace portfolio_api_tests
+{
+    public class ContentfulTests
+    {
+        
+    }
+}
